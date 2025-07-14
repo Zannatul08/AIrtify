76189bf78da3b00f952b0c501c4419a7f83ca172
Zannatul08 <ferdous89001@gmail.com>
2025-07-14 12:56:59 +0600

    Dynamic naming of form headers done

--- app/FormInput.jsx
+++ app/FormInput.jsx
@@ -1,16 +1,29 @@
-import { useLocalSearchParams } from 'expo-router';
-import { useEffect } from 'react';
+import { useLocalSearchParams, useNavigation } from 'expo-router';
+import { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
+import Colors from './../constants/Colors';
 
 export default function FormInput() {
   const params=useLocalSearchParams();
+  const navigation=useNavigation();
+  const [aiModel,setAiModel]=useState();
 
   useEffect(()=>{
     console.log("Params:",params);
+    setAiModel(params)
+    navigation.setOptions({
+      headerShown:true,
+      headerTitle:params?.name 
+
+    })
   },[])
 
   return (
-    <View>
+    <View style={{
+      padding:20,
+      backgroundColor: Colors.WHITE,
+      height:'100%'
+    }}>
       <Text>FormInput</Text>
     </View>
   )
