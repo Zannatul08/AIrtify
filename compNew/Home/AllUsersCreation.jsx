da0c320681c0607b6279a2f92b751671f377e3be
Zannatul08 <ferdous89001@gmail.com>
2025-07-31 01:57:08 +0600

    User generated Images can be downloaded

--- compNew/Home/AllUsersCreation.jsx
+++ compNew/Home/AllUsersCreation.jsx
@@ -1,6 +1,7 @@
-import { useNavigation } from 'expo-router';
+import { useNavigation, useRouter } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { ActivityIndicator, Dimensions, FlatList, Image, Text, View } from 'react-native';
+import { ActivityIndicator, Dimensions, FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
+import Colors from '../../constants/Colors';
 import GlobalApi from '../../services/GlobalApi';
 
 export default function AllUsersCreation() {
@@ -9,11 +10,14 @@ export default function AllUsersCreation() {
   const [startIndex, setStartIndex] = useState(0);
   const [loading, setLoading] = useState(false);
   const [aiImageList, setAiImageList] = useState([]);
-  const ColumnWidth = Dimensions.get('screen').width * 0.9 / 2;
+  const router=useRouter();
+  const ColumnWidth = Dimensions.get('screen').width * 0.86 / 2;
+
 
   const GetAllAiImages = async (start, limit) => {
     try {
       setLoading(true);
+      // setAiImageList([])
       const result = await GlobalApi.GetAllAiImages(start, limit);
       console.log('Fetched Images:', JSON.stringify(result.data, null, 2));
       const resultData = result.data.data || [];
@@ -26,6 +30,7 @@ export default function AllUsersCreation() {
   };
 
   useEffect(() => {
+    setAiImageList([])
     GetAllAiImages(0, pageSize);
     const unsubscribe = navigation.addListener('focus', () => {
       setAiImageList([]); // Clear list to avoid duplicates
@@ -35,14 +40,31 @@ export default function AllUsersCreation() {
     return unsubscribe;
   }, [pageSize]);
 
-  const loadMoreImages = () => {
-    if (!loading) {
-      const newStart = startIndex + pageSize;
-      setStartIndex(newStart);
-      GetAllAiImages(newStart, pageSize);
-    }
-  };
+  // const loadMoreImages = () => {
+  //   if (!loading) {
+  //     const newStart = startIndex + pageSize;
+  //     setStartIndex(newStart);
+  //     GetAllAiImages(newStart, pageSize);
+  //   }
+  // };
+ const RenderFoot=()=>{
+      if(loading)
+      {
+        return <ActivityIndicator size={'large'} color={Colors.PRIMARY}/>
+      }
+      return null;
+ }
 
+
+ const onImageClickHandle=(item)=>{
+  router.push({
+    pathname:'viewAiImage',
+    params:{
+      imageUrl:item.imageUrl,
+      prompt:'Hidden'
+    }
+  })
+ }
   return (
     <View style={{ marginTop: 20 }}>
       <Text style={{ fontSize: 20, fontWeight: 'bold' }}>User's Creation</Text>
@@ -52,25 +74,31 @@ export default function AllUsersCreation() {
         <FlatList
           data={aiImageList}
           numColumns={2}
+          onEndReached={()=>GetAllAiImages(pageSize+5)}
+          onEndReachedThreshold={0.7}
+          ListFooterComponent={RenderFoot}
           keyExtractor={(item) => item.id.toString()}
           renderItem={({ item }) => (
-            <View>
+            <TouchableOpacity
+            onPress={()=>onImageClickHandle(item)}
+            >
               <Image
                 source={{ uri: item.imageUrl }}
                 style={{
                   width: ColumnWidth,
                   height: 250,
                   margin: 5,
+                  borderRadius:15
                 }}
                 onError={(e) =>
                   console.log('Image load error:', item.imageUrl, e.nativeEvent.error)
                 }
               />
-            </View>
+            </TouchableOpacity>
           )}
-          onEndReached={loadMoreImages}
-          onEndReachedThreshold={0.5}
-          ListFooterComponent={loading && aiImageList.length > 0 ? <ActivityIndicator size="large" /> : null}
+          // onEndReached={loadMoreImages}
+          // onEndReachedThreshold={0.5}
+          // ListFooterComponent={loading && aiImageList.length > 0 ? <ActivityIndicator size="large" /> : null}
           ListEmptyComponent={<Text>No images found.</Text>}
         />
       )}
